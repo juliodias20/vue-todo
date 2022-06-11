9dbce3480aa59f2c8445dc03024bfb04c5a81667
juliodias20 <julio_dias@live.com>
2022-06-11 16:27:46 -0300

    Projeto Vue + vuetify + vuex para estudo

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,51 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import Localbase from 'localbase'
+let db = new Localbase('db')
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    tarefas: [
+     // {id:1, titulo: "Ir ao Mercado", concluido:true},
+     // {id:2, titulo: "Comprar ração", concluido:false}
+    ]
   },
   mutations: {
+    buscaTarefas(state) {
+      db.collection('tarefas').get().then(tarefasDB => {
+        state.tarefas = tarefasDB
+      })
+    },
+    adicionaTarefa(state, titulo) {
+      db.collection('tarefas').add({
+        id: new Date().getTime(),
+        titulo,
+        concluido: false
+      })
+    }
   },
   actions: {
+    async adicionaTarefa({commit}, titulo) {
+      await commit('adicionaTarefa', titulo)
+      await commit('buscaTarefas')
+    },
+
+    editaTarefa({commit}, novaTarefa) {
+      db.collection('tarefas').doc({ id: novaTarefa.id }).update({
+        titulo: novaTarefa.titulo
+      }).then(() => {
+        commit('buscaTarefas')
+      })
+    },
+
+    removeTarefa({commit}, id) {
+      db.collection('tarefas').doc({ id }).delete().then(() => {
+        commit('buscaTarefas')
+      })
+    }
   },
   modules: {
   }
